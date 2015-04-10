4ba7300727ff4c7d3eeb9b6ec7ca3d051da8a281
ShanSenanayake <senanayake92@gmail.com>
2015-04-10 15:53:58 +0200

    removed red code

--- QtheMusic/src/test/ServerTest.java
+++ QtheMusic/src/test/ServerTest.java
@@ -6,10 +6,10 @@ import java.net.Socket;
 public class ServerTest {
 
 	public static void main(String[] args) {
-		ServerSocket serverSocket = new ServerSocket(30000);
-		while (true) {
-			Socket user = serverSocket.accept();
-		}
+//		ServerSocket serverSocket = new ServerSocket(30000);
+//		while (true) {
+//			Socket user = serverSocket.accept();
+//		}
 	}
 
 }
