b9bfc4ef9d68f7cf73518f124ad79c041f2509e0
Johan Nyholm <hansjohannyholm@gmail.com>
2015-04-10 11:32:12 +0200

    Split up the server and the client into two separate folders.

---
+++ QtheMusic/CentralServer/Database/ServerTest.java
@@ -0,0 +1,10 @@
+package Database;
+
+public class ServerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello from server");
+	}
+
+}
---
+++ QtheMusic/Client/Test/ClientTest.java
@@ -0,0 +1,6 @@
+package Test;
+public class ClientTest {
+	public static void main(String[] args){
+		System.out.println("hello from client");
+	}
+}
