cd2432d3e5ac935b7a647b1aabd1ad78f9f33158
Johan Nyholm <hansjohannyholm@gmail.com>
2015-04-15 17:34:21 +0200

    Created a class CentralServer for starting the server.

--- QtheMusic/src/Protocol/DebugConstants.java
+++ QtheMusic/src/Protocol/DebugConstants.java
@@ -4,4 +4,5 @@ public class DebugConstants {
 	public static final int HOST_PORT=30000;
 	public static final int SERVER_HOST_PORT=8080;
 	public static final int SERVER_CLIENT_PORT=8090;
+	public static final int MAX_NBR_OF_HOSTS_ALLOWED_BY_SERVER=10;
 }
--- QtheMusic/src/test/CentralServerTest.java
+++ QtheMusic/src/test/CentralServerTest.java
@@ -1,19 +1,12 @@
 package test;
 
 import Protocol.DebugConstants;
-import sun.security.krb5.internal.HostAddress;
-import centralServer.connection.UserClientListener;
-import centralServer.connection.UserHostListener;
-import centralServer.database.ServerMonitor;
+import centralServer.main.CentralServer;
 
 public class CentralServerTest {
-
 	public static void main(String[] args) {
 		System.out.println("hello from Central Server");
-		ServerMonitor monitor = new ServerMonitor(10);
-		UserClientListener ucl = new UserClientListener(monitor, DebugConstants.SERVER_CLIENT_PORT);
-		ucl.start();
-		UserHostListener uhl=new UserHostListener(monitor, DebugConstants.SERVER_HOST_PORT);
-		uhl.start();
+		CentralServer centralServer = new CentralServer(DebugConstants.MAX_NBR_OF_HOSTS_ALLOWED_BY_SERVER);
+		centralServer.run();
 	}
 }
