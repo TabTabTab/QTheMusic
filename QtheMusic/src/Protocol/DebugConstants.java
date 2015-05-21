0b47de717f16bae913d64714325f17dea7d135bc
Denhi Huynh <Denhi_@hotmail.com>
2015-05-21 17:00:21 +0200

    Changed debugconstant

--- QtheMusic/src/Protocol/DebugConstants.java
+++ QtheMusic/src/Protocol/DebugConstants.java
@@ -4,7 +4,7 @@ public class DebugConstants {
 	public static final int HOST_PORT=30000;
 	public static final int SERVER_HOST_PORT=8080;
 	public static final int SERVER_CLIENT_PORT=8090;
-	public static final String CENTRAL_SERVER_IP="192.168.1.88";
+	public static final String CENTRAL_SERVER_IP="192.168.43.253";
 	public static final int MAX_NBR_OF_HOSTS_ALLOWED_BY_SERVER=10;
 	public static final int MAX_NBR_OF_CLIENTS_ALLOWED_BY_HOST=10;
 }
